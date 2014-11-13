20ae717b18ec5620415849023c0e0580f9566d9f
RobotEve <ksw2008@hotmail.co.uk>
2014-11-13 10:04:16 +0000

    SMILES_bin_matrix.m
    Create the binary matrix in Octave

---
+++ SMILES_bin_matrix.m
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% create the binary matrix in Octave
+%
+% code for use in windows (unix code differences given in comment form)
+%
+% Kevin Williams 25 Feb 2011
+% adapted from Kurt's email dated 14 Nov 2010
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+cd "c:/kswfiles/cherrypick_march"
+compoundLibrary = load("AllSMILES_fptmatrix.ascii");
+% unix: compoundLibrary=load AllSMILES_fptmatrix.ascii
+compoundLibrary = compoundLibrary > 0; % create compact logical matrix
+save -binary AllSMILES_fptmatrix.bin compoundLibrary;
